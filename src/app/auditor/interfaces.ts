9b297d52fd4123292a297e78d346d6ec63e31c5f
fdrodriguez <fdrodriguez@indra.es>
2023-10-28 17:28:34 +0200

    configuracion irltmm

---
+++ src/app/auditor/interfaces.ts
@@ -0,0 +1,16 @@
+export interface ITrackCircuit {
+    id: string;
+    mnemonic: string;
+    name: string;
+    direction: string;
+    type: string;
+    nodeName: string;
+    nodeMnemonic: string;
+    arcName: string;
+    arcMnemonic: string;
+    trainDetectorMnemonic: string;
+    circulationTrackMnemonic: string;
+    stationingTrackMnemonic: string;
+    tczName: string;
+    tczMnemonic: string;
+}
