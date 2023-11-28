cc49e137a312b54554ed30f219c3c099e8ec2450
fdrodriguez <fdrodriguez@indra.es>
2023-11-28 22:57:28 +0100

    configuración tmslocal

---
+++ src/app/environments/environments.tmslocal.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    name: "TMSLOCAL",
+    authUrl: "https://keycloak20-test.apps.k8s.mova.indra.es",
+    realtimeUrl: "http://localhost:9082",
+    dataaccessUrl: "http://localhost:9080",
+    topologyUrl: "http://localhost:9090",
+    topologyVersion: "ER_TOPO_GLOBAL_3_1_6"
+}
