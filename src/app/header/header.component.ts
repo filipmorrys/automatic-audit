f8b60c916a8773c43332db43334a240c2fdf1153
fdrodriguez <fdrodriguez@indra.es>
2023-12-04 17:42:42 +0100

    Estilos de bootstrap

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { environment } from '../environments/environments';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   env: string = environment.name;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
 }
